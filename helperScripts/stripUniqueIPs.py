c0e2abf0be25d338aafc5e25d2d75e92ce56637b
J. Renner <j@lerenner.dev>
2022-08-11 20:58:06 -0300

    Fix indexing error in helper script
    Signed-off-by: J. Renner <j@lerenner.dev>

--- helperScripts/stripUniqueIPs.py
+++ helperScripts/stripUniqueIPs.py
@@ -6,7 +6,7 @@ output = open("extracted/uniqueIPs.txt", "w")
 for linha in completeLog:
   splitLine = linha.split()
   
-  if   splitLine[9] == "user": ip = splitLine[11]
+  if   splitLine[9] == "user": ip = splitLine[12]
   elif splitLine[9] == "from": ip = splitLine[10]
   
   if ip not in ips:
