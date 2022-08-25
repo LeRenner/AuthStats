ed065c8860ff646197f11a36264229cf76055462
J. Renner <j@lerenner.dev>
2022-08-25 01:02:09 -0300

    Make script output more verbose
    Signed-off-by: J. Renner <j@lerenner.dev>

--- 2-stripIPsAndUsernames.sh
+++ 2-stripIPsAndUsernames.sh
@@ -1,13 +1,22 @@
 # First, lets extract all relevant lines from log
 mkdir extracted
 
+echo "Unpacking logs..."
 gunzip rawLogs/*.gz
 
 cat rawLogs/* > extracted/completeLog.txt
 
+echo "Extracting relevant part of logs..."
 cat extracted/completeLog.txt | grep "Invalid user" > extracted/invalidUserLogs.txt
 cat extracted/completeLog.txt | grep "Failed password for" > extracted/failedPasswordAttempts.txt
 
 # extract unique IPs and usernames
+echo -e "\nStarting IP stripping script..."
+echo "###########"
 python3 helperScripts/stripUniqueIPs.py
+echo "###########"
+
+echo -e "\nStarting Username stripping script..."
+echo "###########"
 python3 helperScripts/stripUniqueUsernames.py
+echo "###########"
