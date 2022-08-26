42a878c9836a7fa976ab16d5f89307202ddb997f
J. Renner <j@lerenner.dev>
2022-08-26 08:12:09 -0300

    Add edge cases to queryCountries.py
    Signed-off-by: J. Renner <j@lerenner.dev>

--- helperScripts/queryCountries.py
+++ helperScripts/queryCountries.py
@@ -2,16 +2,19 @@ import sys
 import iptocc
 import logging
 import json
+from math import isnan
 
 index = 0
 ips = open("extracted/uniqueIPs.txt", "r").readlines()
-countries = {}
+countries = {"NaN": 0}
 logger = logging.getLogger("iptocc")
 logger.propagate = False
 
+# Initialize IP db
 print("Setting up IP range database...")
 iptocc.get_country_code("8.8.8.8")
 print("Querying IP addresses...")
+
 
 for ip in ips:
 
@@ -20,7 +23,15 @@ for ip in ips:
     sys.stdout.write("\rProgress - %s%%" % str(round(100 * index/len(ips), 2)))
     sys.stdout.flush()
 
-  originCountry = iptocc.get_country_code(ip[:-1])
+  # Handle invalid IP address
+  try: originCountry = iptocc.get_country_code(ip[:-1])
+  except: print("Error parsing " + ip + ". Skipping.")
+
+  # Handle country not found
+  if type(originCountry) != type("A"):
+    countries["NaN"] += 1
+    continue
+
   if originCountry not in countries:
     countries[originCountry] = 1
     continue
@@ -32,11 +43,14 @@ for ip in ips:
 
 print("\rProgress - Done!")
 
+# Sort countries by order of appearence
 sorted = dict(sorted(countries.items(), reverse=True))
 output = open("extracted/countryStats.json", "w")
 
+# Save json output
 jsonOutput = json.dumps(sorted, indent = 4)
 output.write(jsonOutput)
 
+# Print results on screen
 for key in sorted:
   print("Number of IPs from", key, "-", countries[key])
